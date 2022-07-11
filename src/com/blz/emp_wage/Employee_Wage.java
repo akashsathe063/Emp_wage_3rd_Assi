ee4473e005be5251af5a1bf74f986320d4637002
akashsathe063 <akashsathe063@gmail.com>
2022-07-11 18:30:38 +0530

    Employee Attendence

--- src/com/blz/emp_wage/Employee_Wage.java
+++ src/com/blz/emp_wage/Employee_Wage.java
@@ -1,8 +1,16 @@
 package com.blz.emp_wage;
 
 public class Employee_Wage {
+    static  int isPresent = 1;
     public static void main(String[] args) {
-        System.out.println("Welcome to Employee wage computation");
+        System.out.println("Well come to Employee Wage computation");
+        int checkRandom = (int) Math.floor(Math.random() * 10) % 2;
+        if(checkRandom == isPresent){
+            System.out.println("Employee is present");
+        }
+        else{
+            System.out.println("Employee is Absent");
+        }
     }
 }
 
