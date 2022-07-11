c0e035324dddf87045b890cd523301d0cb1d8545
akashsathe063 <akashsathe063@gmail.com>
2022-07-11 19:06:41 +0530

    total working hours and day is reached for a month

--- src/com/blz/emp_wage/Employee_Wage.java
+++ src/com/blz/emp_wage/Employee_Wage.java
@@ -1,38 +1,46 @@
 package com.blz.emp_wage;
 
 public class Employee_Wage {
-    static final int isPresent = 1;
-    static final int isPartime_present = 2;
+    static int isPresent = 1;
+    static int isPartime_present = 2;
     static int working_day_per_month = 20;
+    static int max_working_hours = 100;
     static int wage_per_hour = 20;
     static int full_day_hours = 8;
     static int part_time_hours = 4;
     static int wage = 0;
     static int total_wage = 0;
+    static  int total_hours = 0;
 
     public static void main(String[] args) {
         System.out.println("Well come to Employee Wage computation");
-        for (int day = 1; day <= working_day_per_month; day++) {
+        int day = 0;
+        while(total_hours < max_working_hours &&  day<= working_day_per_month ) {
             int checkRandom = (int) Math.floor(Math.random() * 10) % 3;
             switch (checkRandom) {
-                case isPresent:
-                    System.out.println("Employee is present: " + day);
+                case 1:if(total_hours + full_day_hours < max_working_hours) {
+                    System.out.println("Employee is present and day of month: " + day++);
                     wage = wage_per_hour * full_day_hours;
                     total_wage = total_wage + wage;
+                    total_hours = total_hours + full_day_hours;
+                }
                     break;
-                case isPartime_present:
-                    System.out.println("Employee is part time present: " + day);
-                    wage = wage_per_hour * part_time_hours;
-                    total_wage = total_wage + wage;
+                case 2:
+                    if(total_hours + part_time_hours < max_working_hours) {
+                        System.out.println("Employee is part time present and day of month: " + day++);
+                        wage = wage_per_hour * part_time_hours;
+                        total_wage = total_wage + wage;
+                        total_hours = total_hours + part_time_hours;
+                    }
                     break;
                 default:
-                    System.out.println("Employee is absent: " + day);
+                    System.out.println("Employee is absent and day of month: " + day++);
                     break;
 
             }
 
         }
-
+        System.out.println("Total Hours: "+total_hours);
         System.out.println("Month Employee Wage: " + total_wage);
     }
 }
